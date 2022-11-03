173c353443aa9c6820c3798f6c0b7e55c20bd666
KawhiKobeJordan <117371328+KawhiKobeJordan@users.noreply.github.com>
2022-11-03 09:50:43 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,96 @@
+# name, ass, titties, thick, teeth, personality, freak, money, kids, talker,
+# Old_Con(connection), FWM, safety(no tell safe puss)
+
+# TaSo KoBa JaBa DeWh BrRe KeHa TeBl MiPr KaTh CyTh DaWh KiMa KeLe Syp KhMa BeMa SyEv ReSh
+# BPratt JhMo lANDA ShMa LaJe TaSn SyG JaSt Aniya
+
+safe_nut = {'KoBa': 1, 'ReSh': 1, 'TaSn': 1, 'SyP': 1, 'ShMa': 1, 'lANDA': 0, 'BeMa': 1, 'SyEv': 1, 'KeHa': 0,
+            'JaSt': 0, 'TeBl': 1, 'KeLe': 1, 'LaJe': 1, 'MiPr': 1, 'Aniya': 1, 'BrRe': 1, 'DeWh': 1, 'TaSo': 1,
+            'KhMa': 1, 'CyTh': 0, 'SyG': 1, 'KaTh': 0, 'KiMa': 1, 'JhMo': 1, 'DaWh': 1, 'BPratt': 1, 'JaBa': 0}
+
+
+trust_em = {'KoBa': 1, 'ReSh': 1, 'TaSn': 1, 'SyP': 1, 'ShMa': 1, 'lANDA': 1, 'BeMa': 1, 'SyEv': 0, 'KeHa': 0,
+            'JaSt': 0, 'TeBl': 1, 'KeLe': 1, 'LaJe': 1, 'MiPr': 0, 'Aniya': 1, 'BrRe': 1, 'DeWh': 1, 'TaSo': 0,
+            'KhMa': 1, 'CyTh': 0, 'SyG': 1, 'KaTh': 0, 'KiMa': 1, 'JhMo': 1, 'DaWh': 1, 'BPratt': 1, 'JaBa': 0}
+
+connect = {'KoBa': 1, 'ReSh': 0, 'TaSn': 1, 'SyP': 1, 'ShMa': 1, 'lANDA': 0, 'BeMa': 0, 'SyEv': 1, 'KeHa': 1,
+           'JaSt': 0, 'TeBl': 1, 'KeLe': 1, 'LaJe': 0, 'MiPr': 1, 'Aniya': 1, 'BrRe': 1, 'DeWh': 0, 'TaSo': 1,
+           'KhMa': 0, 'CyTh': 1, 'SyG': 0, 'KaTh': 1, 'KiMa': 0, 'JhMo': 1, 'DaWh': 1, 'BPratt': 1, 'JaBa': 1}
+
+talker = {'KoBa': 0, 'ReSh': 0, 'TaSn': 1, 'SyP': 0, 'ShMa': 0, 'lANDA': 0, 'BeMa': 1, 'SyEv': 0, 'KeHa': 1,
+          'JaSt': 1, 'TeBl': 0, 'KeLe': 1, 'LaJe': 1, 'MiPr': 1, 'Aniya': 0, 'BrRe': 0, 'DeWh': 0, 'TaSo': 1,
+          'KhMa': 0, 'CyTh': 1, 'SyG': 0, 'KaTh': 0, 'KiMa': 1, 'JhMo': 1, 'DaWh': 0, 'BPratt': 1, 'JaBa': 1}
+
+jits = {'KoBa': 0, 'ReSh': 0, 'TaSn': 1, 'SyP': 0, 'ShMa': 1, 'lANDA': 0, 'BeMa': 0, 'SyEv': 0, 'KeHa': 0,
+        'JaSt': 0, 'TeBl': 0, 'KeLe': 0, 'LaJe': 0, 'MiPr': 1, 'Aniya': 0, 'BrRe': 0, 'DeWh': 0, 'TaSo': 1,
+        'KhMa': 0, 'CyTh': 0, 'SyG': 0, 'KaTh': 0, 'KiMa': 0, 'JhMo': 0, 'DaWh': 0, 'BPratt': 0, 'JaBa': 0}
+
+
+teeth = {'KoBa': 1, 'ReSh': 1, 'TaSn': 1, 'SyP': 1, 'ShMa': 1, 'lANDA': 1, 'BeMa': 1, 'SyEv': 1, 'KeHa': 1,
+         'JaSt': 1, 'TeBl': 1, 'KeLe': 1, 'LaJe': 1, 'MiPr': 1, 'Aniya': 1, 'BrRe': 1, 'DeWh': 0, 'TaSo': 1,
+         'KhMa': 1, 'CyTh': 1, 'SyG': 1, 'KaTh': 1, 'KiMa': 1, 'JhMo': 0, 'DaWh': 1, 'BPratt': 1, 'JaBa': 1}
+
+eater = {'KoBa': 1, 'ReSh': 1, 'TaSn': 1, 'SyP': 1, 'ShMa': 1, 'lANDA': 1, 'BeMa': 0, 'SyEv': 1, 'KeHa': 1,
+         'JaSt': 1, 'TeBl': 1, 'KeLe': 1, 'LaJe': 0, 'MiPr': 1, 'Aniya': 0, 'BrRe': 1, 'DeWh': 0, 'TaSo': 1,
+         'KhMa': 0, 'CyTh': 1, 'SyG': 0, 'KaTh': 0, 'KiMa': 1, 'JhMo': 0, 'DaWh': 0, 'BPratt': 0, 'JaBa': 1}
+
+persona = dict(KoBa=1, ReSh=0, TaSn=1, SyP=1, ShMa=1, lANDA=1, BeMa=0, SyEv=1, KeHa=1, JaSt=0, TeBl=1, KeLe=1, LaJe=0,
+               MiPr=0, Aniya=0, BrRe=1, DeWh=0, TaSo=1, KhMa=1, CyTh=1, SyG=0, KaTh=1, KiMa=0, JhMo=0, DaWh=0, BPratt=1,
+               JaBa=1)
+
+thickees = {'KoBa': 1, 'ReSh': 1, 'TaSn': 1, 'SyP': 1, 'ShMa': 1, 'lANDA': 1, 'BeMa': 1, 'SyEv': 1, 'KeHa': 1,
+            'JaSt': 0, 'TeBl': 1, 'KeLe': 1, 'LaJe': 0, 'MiPr': 0, 'Aniya': 0, 'BrRe': 0, 'DeWh': 0, 'TaSo': 1,
+            'KhMa': 0, 'CyTh': 1, 'SyG': 0, 'KaTh': 0, 'KiMa': 0, 'JhMo': 0, 'DaWh': 0, 'BPratt': 0, 'JaBa': 1}
+
+ass_rank = {'KoBa': '1st', 'ReSh': '2nd', 'TaSn': '3rd', 'Syp': '4th', 'ShMa': '5th', 'lANDA': '6th', 'BeMa': '7th',
+            'SyEv': '8th', 'KeHa': '9th', 'JaSt': '10th', 'TeBl': '11th', 'DeWh': '12th',
+            'TaSo': '13th', 'JaBa': '14th', 'KeLe': '15th', 'LaJe': '16th', 'MiPr': '17th', 'Aniya': '18th',
+            'BrRe': '19th', 'KhMa': '20th', 'CyTh': '21st', 'SyG': '22nd', 'KaTh': '23rd', 'KiMa': '24th',
+            'BPratt': '25th', 'JhMo': '26th', 'DaWh': '27th'}
+
+melons = ['KoBa', 'DeWh', 'JaBa', 'JaSt', 'BeMa', 'ShMa', 'BrRe', 'KhMa', 'LaJe', 'CyTh', 'JhMo', 'KeHa', 'TaSo',
+          'KeLe', 'TaSn', 'SyEv', 'ReSh', 'BPratt', 'lANDA', 'Aniya', 'KaTh', 'SyG', 'MiPr', 'KiMa', 'SyP', 'DaWh',
+          'TeBl']
+
+
+# def is_fat():
+    #cheeked_up = ass_rank.values()
+    #print(f"Rank: {cheeked_up}')
+
+   # for booty in ass_rank:
+   #     booty += 1
+
+  #  for booty in ass_rank:
+   #     if booty.dict.value() < 16:
+   #         print("That ass fat af")
+#    else:
+  #      print(False)
+
+
+#def big_booty():
+    #return is_fat
+
+
+class Contestant:
+    def __init__(self, name, ass, titties, thick, personality, freak, teeth, money, kids, talker, old_con):
+        def name(self):
+            self.name = name
+
+        def ass():
+
+
+        self.name = name
+        self.ass = ass
+        self.titties = titties
+        self.thick = thick
+        self.personality = personality
+        self.freak = freak
+        self.teeth = teeth
+        self.money = money
+        self.kids = kids
+        self.talker = talker
+        self.old_con = old_con
+
+
+TaSo = Contestant('TaSo')
+TaSo.name
